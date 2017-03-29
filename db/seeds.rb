d414d8593ad9952c403d1b5d5f0589c4b6fa3c0e
Shawzt <shawzt@gmail.com>
2017-03-29 20:26:44 +0800

    DB seeds for demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do |i|
+  User.create(username: "Demo#{i}", email: "demo#{i}@gmail.com", password: '111111')
+end
